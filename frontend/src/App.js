bb74ffeec92801e6b6f0cd85ad575d17d60a51b3
m-triple-m <triplem656@gmail.com>
2022-04-14 18:37:55 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/main/login";
+import Admin from "./components/admin";
+import AdminProfile from "./components/admin/profile";
+import Main from "./components/main";
+import User from "./components/user";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Admin />} path="admin">
+          <Route element={<AdminProfile />} path="profile" />
+        </Route>
+
+        <Route element={<Main />} path="main"></Route>
+
+        <Route element={<User />} path="user"></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/components/admin/index.js
+++ frontend/src/components/admin/index.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Admin = () => {
-  return <div>Admin</div>;
+  return (
+    <div>
+      <h1>Admin Component</h1>
+      <Outlet />
+    </div>
+  );
 };
 
 export default Admin;
